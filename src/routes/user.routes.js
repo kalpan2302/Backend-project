1b7849de60a0aee14fc90ef5f1ed3ca40f0ea4e1
kalpan <kalpan.s.23@gmail.com>
2024-04-03 14:33:23 +0530

    login logout controller, access and refresh token, authorization middleware , cookies

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { logOutUser, loginUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
  const router = Router()
 
@@ -16,6 +17,12 @@ import {upload} from "../middlewares/multer.middleware.js"
         }
     ]),
     registerUser
-    )
+)
+
+router.route("/login").post(loginUser)
+
+//secured routes
+
+router.route("/logout").post(verifyJwt, logOutUser)
 
  export default router;
